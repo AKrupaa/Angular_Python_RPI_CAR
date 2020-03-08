b55fd7de7717c2d5e2d56942f86a583c12155939
Arkadiusz <arkadiusz.krupinski15@gmail.com>
2020-03-08 13:49:21 +0100

    Metody - wyjaśnienie

--- Angular/AngularApplication/src/app/all-in-all-tutorial-with-comments/all-in-all-tutorial-with-comments.component.ts
+++ Angular/AngularApplication/src/app/all-in-all-tutorial-with-comments/all-in-all-tutorial-with-comments.component.ts
@@ -7,24 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllInAllTutorialWithCommentsComponent implements OnInit {
 
-  date_message:string;
+
   //  zmienna przechowująca aktualną date oraz czas (patrz konstruktor -> setInterval())
+  date_message:string;
+  
+  // zmienna przechowująca liczbę 10
+  someNumber: number = 10;
 
-  constructor() { 
-
-    setInterval(() => {
-      let currentDate = new Date();
-      this.date_message = currentDate.toDateString() + ' ' + currentDate.toTimeString();
-    }, 1000);
+  constructor() {
 
     // async operation (`setInterval`) to dynamically update the date component's displayed value, 
     // so that it shows updates to the time value every second.
     // setInterval -> powoduje odświeżanie obiektu currentDate oraz date_message 
     // w celu wyświetlania co 1000ms czyli co jedną sekundę aktualnego czasu ustawionego w systemie
-    
+
+    setInterval(() => {
+      let currentDate = new Date();
+      this.date_message = currentDate.toDateString() + ' ' + currentDate.toTimeString();
+    }, 1000);
   }
 
   ngOnInit(): void {
   }
+
+  // metoda addTwoNumbers(a: number, b: number) pozwala dodać dwie liczby
+  // aby wywołać ją w .html
+  // należy wpisać:
+  // {{ addTwoNumbers(1, 100) }}
+  // wtedy w .HTML pojawi nam się liczba 101
+  // można ją również wywołać w ten sposób: 
+  // {{ addTwoNumbers(someNumber, 1) }}
+  // zwrócony wynik to 11, ponieważ zmienna someNumber wynosi 10
+
+  addTwoNumbers(a: number, b: number) {
+    return a + b;
+  }
+
+  // metoda someOtherMethod() wywołuje metodę addTwoNumbers(..., ...) TEJ klasy
+  // aby wywołać ją w .HTML wystarczy {{ someOtherMethod() }}
+  someOtherMethod() {
+    this.addTwoNumbers(this.someNumber, 12);
+  }
+
 
 }
